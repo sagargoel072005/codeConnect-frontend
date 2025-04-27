8e8498781018934bbe49e537deb6438cae9b355b
sagargoel072005 <goels4280@gmail.com>
2025-04-27 22:59:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1 class="text-3xl font-bold underline text-center text-gray-800 ">
+          Hello world!
+        </h1>
+    </>
+  )
+}
+
+export default App;
