f5790bdbf32d3ac4cb85c67af2f3e56b45f0a931
sagargoel072005 <goels4280@gmail.com>
2025-07-31 17:05:52 +0530

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
-
+  build: {
+     chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: () => 'everything.js',
+      },
+    },
+  },
+});
